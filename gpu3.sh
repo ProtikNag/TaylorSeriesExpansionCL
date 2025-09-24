225a899ece33961fa258a2fccdf202ee50f860c6
ProtikNag <protiknag08@gmail.com>
2025-09-24 01:02:09 -0400

    pv run 002

--- gpu3.sh
+++ gpu3.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#SBATCH --job-name=taylor
+#SBATCH --job-name=der
 #SBATCH -N 1         			## Compute None (Number of computers)
 #SBATCH -n 24 	     			## CPU Cores 
 #SBATCH --gres=gpu:2 			## Run on 2 GPUs
 #SBATCH --output job%j.%N.out
 #SBATCH --error job%j.%N.err
-#SBATCH -p gpu		
+#SBATCH -p gpu
 
 hostname
 date
