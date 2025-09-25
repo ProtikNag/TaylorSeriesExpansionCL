cf1551833fb52b30b39b386cf3e87bd600ce8d3a
ProtikNag <protiknag08@gmail.com>
2025-09-25 01:05:04 -0400

    Temporary

--- methods/taylor.py
+++ methods/taylor.py
@@ -37,8 +37,8 @@ def select_best_permutation(base_model, task_group_ids, train_loaders, val_loade
 
 
 def taylor_global_update(global_model, local_model, train_loader, lambda_reg=100.0, device='cuda'):
-    # global_model.load_state_dict(local_model.state_dict())
-    # return global_model
+    global_model.load_state_dict(local_model.state_dict())
+    return global_model
     criterion = nn.CrossEntropyLoss()
     global_model.train()
     local_model.eval()
