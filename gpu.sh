3e02750a5c5c1296f24b9acb6983f532d6fce2c2
ProtikNag <protiknag08@gmail.com>
2025-09-24 20:42:42 -0400

    Temporary

--- gpu.sh
+++ gpu.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:2 			## Run on 2 GPUs
 #SBATCH --output job%j.%N.out
 #SBATCH --error job%j.%N.err
-#SBATCH -p gpu
+#SBATCH -p gpu-v100-32gb
 
 hostname
 date
@@ -18,4 +18,4 @@ source activate /work/pnag/envs/ml_env/
 python --version
 
 cd /work/pnag/TaylorSeriesExpansionCL/
-python main.py --dataset SplitMNIST --num_tasks 5 --group_size 2 --epochs 5 --lr 0.01 --methods taylor lambda_reg 500
+python main.py --dataset SplitMNIST --num_tasks 5 --group_size 2 --epochs 5 --lr 0.01 --methods taylor lambda_reg 5
