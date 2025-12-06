ac8d3295debf7b0daf17df8a2835283332a471ca
Protik Nag <protiknag08@gmail.com>
2025-12-06 12:40:15 -0500

    Dec 6: HPC and Local aligned

--- gpu.sh
+++ gpu.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1 			## Run on 2 GPUs
 #SBATCH --output job%j.%N.out
 #SBATCH --error job%j.%N.err
-#SBATCH -p gpu
+#SBATCH -p gpu-v100-32gb
 
 hostname
 date
@@ -18,4 +18,4 @@ source activate /work/pnag/envs/ml_env/
 python --version
 
 cd /work/pnag/TaylorSeriesExpansionCL/
-python main.py --dataset SplitMNIST --num_tasks 5 --group_size 2 --epochs 5 --lr 0.01
+python main.py
